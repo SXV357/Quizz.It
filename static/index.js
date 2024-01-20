018d0818012b619ae413f9655ba17ae02d837206
Shreyas Viswanathan <shrevis2018@gmail.com>
2024-01-20 12:24:20 -0500

    fix file upload bug

--- static/index.js
+++ static/index.js
@@ -1,26 +1,21 @@
-var file_upload_status = document.querySelector(".file_upload_status")
-var file_input = document.querySelector("#file_input")
-var upload_button = document.querySelector("#upload")
-var formData = new FormData();
+document.addEventListener("DOMContentLoaded", function() {
+    var file_upload_status = document.querySelector(".file_upload_status");
+    var file_input = document.querySelector("#upload");
+    var upload_form = document.querySelector("#uploadForm");
+    
 
-upload_button.addEventListener("submit", (e) => {
-    e.preventDefault();
-    formData.append("upload", file_input.files[0]);
-    fetch("http://127.0.0.1:5000/upload_file", {
-        method: "POST",
-        body: formData
-    })
-    .then((res) => res.json())
-    .then((data) => {
-        file_upload_status.innerHTML = data.status;
-    })
-    .catch((err) => console.log(err));
+    upload_form.addEventListener("submit", (e) => {
+        e.preventDefault(); // Prevent the default form submission
+        var formData = new FormData();
+        formData.append("upload", file_input.files[0]);
+        fetch("http://127.0.0.1:5000/upload_file", {
+            method: "POST",
+            body: formData
+        })
+        .then((res) => res.json())
+        .then((data) => {
+            file_upload_status.innerHTML = data.status;
+        })
+        .catch((err) => console.log(err));
+    });
 });
-
-function get_summary() {
-
-} 
-
-function generate_pdf() {
-
-}
